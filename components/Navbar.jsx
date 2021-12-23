e2166f7af6cde153053ef895c154dc488b1eca32
nozky <norvillie.villaruel@edu.sait.ca>
2021-12-23 10:35:57 -0700

    fix in importing link compoent in navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react'
-import Link from 'next/Link'
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/navbar.module.css'
 
