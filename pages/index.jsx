1a479456d6a98bbad39c6e56b0cf12a73d0f5851
nozky <norvillie.villaruel@edu.sait.ca>
2021-12-27 10:04:50 -0700

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
 import styles from '../styles/Home.module.css'
 import { hero_images } from '../data/hero_images'
 import Slide from '../components/Slide'
@@ -14,9 +15,9 @@ export default function Home() {
         <title>Baraka ni kalantyaw | Home</title>
         <meta name="description" content="Aklan's People and Culture" />
         <link rel="icon" href="/favicon.ico" />
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
 
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
 
       <section className={ styles.hero }>
         {hero_images.map((image,idx) => idx == selectedIdx? <Slide key={image.title} image={image} /> : null)}
